6a327c0684a0fbdbe9a8dd81986270ef5a63bde7
HyunheungTerryCho <hcho226@umd.edu>
2025-02-26 22:28:09 +0000

    hyunheungCho_ex5

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,27 @@
 // Add your code here
+#include <iostream>
+using namespace std;
+
+int factorial(int a) 
+{
+    int r = 1;  // Initialize result
+
+    for (int i = 1; i <= a; i++) { // Adding +1 to i until it reaches integer a
+        r = r * i;  // Compute factorial
+    }
+
+    return r;  
+}
+
+int main() {
+  
+      // Variable to store data given by cin
+    int a;
+
+    // Take input using cin
+    cout << "Please enter an integer value: ";
+    cin >> a;
+
+    cout << "The value you entered is " << a << " and its factorial is " << factorial(a) << endl;
+    return 0;
+}
