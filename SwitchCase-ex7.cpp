aee848d7d8fc96aee789ced60f8a99c69c7c4133
HyunheungTerryCho <hcho226@umd.edu>
2025-02-27 02:14:09 +0000

    hyunheungcho_ex8

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,35 @@
-// add your code here
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, i;
+
+    // Get user input
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // Assign values to 'i' for three cases
+    if (num % 2 == 0 && num < 10) {
+        i = 1;  // This is going to be case 1. i is even and less than 10.
+    } else if (num % 2 != 0) {
+        i = 2;  // This is going to be case 2. i is odd because of no remainder.
+    } else {
+        i = 3;  // Everything else (hahaha case)
+    }
+
+    switch (i) {
+        case 1: // Even and less than 10 case
+            cout << "The square of " << num << " is " << num * num << endl;
+            break;
+
+        case 2: // Odd number case
+            cout << num << " divided by 2 is " << num / 2.0 << endl;
+            break;
+
+        case 3: // All other cases
+            cout << "hahaha" << endl;
+            break;
+    }
+
+    return 0;
+}
